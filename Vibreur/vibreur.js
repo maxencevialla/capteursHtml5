bfa0a19d6e19f479ea1cb71b98ce5642d8dfc9ba
StephaneHiller <stephane-hiller@etud.univ-tln.fr>
2017-12-14 15:53:33 +0100

    small modif vibreur.js

--- Vibreur/vibreur.js
+++ Vibreur/vibreur.js
@@ -1,3 +1,3 @@
 function vibre() {
-  window.navigator.vibrate(2000); //Vibre pendant 2 secondes
+  window.navigator.vibrate([100,30,100,30,100,200,200,30,200,30,200,200,100,30,100,30,100]); // Vibrate 'SOS' in Morse.
 };
